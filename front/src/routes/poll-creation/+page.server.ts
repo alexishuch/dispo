5f946f30d60f8e7302c1e1d8b1bf71980883eb57
Alexis Huchet <81961494+alexishuch@users.noreply.github.com>
2026-02-11 10:33:48 +0100

    func: Redirect to new poll once created

--- front/src/routes/poll-creation/+page.server.ts
+++ front/src/routes/poll-creation/+page.server.ts
@@ -1,5 +1,6 @@
 import { createPoll } from "$lib/api/polls";
-import { fail, type Actions } from "@sveltejs/kit";
+import type { IPoll } from "$lib/model";
+import { fail, redirect, type Actions } from "@sveltejs/kit";
 
 export const actions: Actions = {
     create: async ({ request, fetch }) => {
@@ -7,12 +8,16 @@ export const actions: Actions = {
         const name = String(formData.get('name'));
         const start_date = String(formData.get('start_date'));
         const end_date = String(formData.get('end_date'));
+        let newPoll: IPoll;
 
         try {
-            return createPoll(name, start_date, end_date);
+            newPoll = await createPoll(name, start_date, end_date);
         } catch (error: unknown) {
             const errorMessage = error instanceof Error ? error.message : String(error);
+            console.error('Error creating poll:', error);
             return fail(422, { error: errorMessage });
         }
+
+        redirect(303, '/poll/' + newPoll.id);
     }
 };
