1c1d832a71be7680a9b5d08172f13f3365d82b55
Alexis Huchet <81961494+alexishuch@users.noreply.github.com>
2026-03-03 11:48:55 +0100

    chore: Request poll only from server

--- front/src/routes/poll/[id]/+page.server.ts
+++ front/src/routes/poll/[id]/+page.server.ts
@@ -1,6 +1,7 @@
 import { getPoll } from "$lib/api/polls";
 
 export const load = async ({ params, fetch }) => {
+    console.log("Loading !")
     const poll = await getPoll(params.id, fetch);
     return { poll };
 };
