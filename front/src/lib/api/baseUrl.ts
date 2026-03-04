59f0ff8ba4de14ba00839fb8e4882fc61ff51961
Alexis Huchet <81961494+alexishuch@users.noreply.github.com>
2026-03-04 15:23:30 +0100

    chore: Edit baseUrl to ease local development

--- front/src/lib/api/baseUrl.ts
+++ front/src/lib/api/baseUrl.ts
@@ -3,16 +3,15 @@ import {
     env
 } from '$env/dynamic/public';
 
+
 function getApiBaseUrl(): string {
+    if (browser) {
+        return dev ? `http://${window.location.hostname}:3000` : env.PUBLIC_API_BASE_URL;
+    }
     if (dev) {
         return 'http://localhost:3000'
     }
-
-    if (!browser) {
-        return 'http://dispo_nestjs:3000';
-    }
-
-    return env.PUBLIC_API_BASE_URL;
+    return 'http://dispo_nestjs:3000'
 }
 
 export const API_BASE_URL = getApiBaseUrl();
