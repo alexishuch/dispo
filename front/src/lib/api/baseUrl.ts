1dfe0be710ec34bf6321b276c571db4e7a4ff247
Alexis Huchet <81961494+alexishuch@users.noreply.github.com>
2026-03-06 23:25:25 +0100

    refactor: Create separate env variable for internal API url

--- front/src/lib/api/baseUrl.ts
+++ front/src/lib/api/baseUrl.ts
@@ -5,12 +5,12 @@ function getApiBaseUrl(): string {
   if (browser) {
     return dev
       ? `http://${window.location.hostname}:3000`
-      : env.PUBLIC_API_BASE_URL;
+      : env.PUBLIC_API_URL;
   }
   if (dev) {
     return 'http://localhost:3000';
   }
-  return 'http://nestjs:3000';
+  return env.PUBLIC_INTERNAL_API_URL;
 }
 
 export const API_BASE_URL = getApiBaseUrl();
