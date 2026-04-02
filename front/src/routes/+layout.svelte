282a0ddf364dc66806be707ae9264a48328d0d7b
Alexis Huchet <81961494+alexishuch@users.noreply.github.com>
2026-04-02 15:25:39 +0200

    chore: Add meta + cover in svelte:head

--- front/src/routes/+layout.svelte
+++ front/src/routes/+layout.svelte
@@ -28,6 +28,42 @@
   });
 </script>
 
+<svelte:head>
+  <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+  <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+  <link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+  <meta name="apple-mobile-web-app-title" content="Dispo?" />
+  <link rel="manifest" href="/site.webmanifest" />
+
+  <title>Dispo?</title>
+  <meta name="title" content="Dispo?" />
+  <meta
+    name="description"
+    content="L'app de sondage ultra simple pour savoir quand se capter !"
+  />
+
+  <!-- Open Graph / Facebook -->
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://dispo.la/" />
+  <meta property="og:title" content="Dispo?" />
+  <meta
+    property="og:description"
+    content="L'app de sondage ultra simple pour savoir quand se capter !"
+  />
+  <meta property="og:image" content="https://dispo.la/cover.png" />
+
+  <!-- X (Twitter) -->
+  <meta property="twitter:card" content="summary_large_image" />
+  <meta property="twitter:url" content="https://dispo.la/" />
+  <meta property="twitter:title" content="Dispo?" />
+  <meta
+    property="twitter:description"
+    content="L'app de sondage ultra simple pour savoir quand se capter !"
+  />
+  <meta property="twitter:image" content="https://dispo.la/cover.png" />
+</svelte:head>
+
 <header>
   <div>
     <a href="/">
